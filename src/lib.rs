ff9dfab444fe4f188cca0212f8ef78ad95c7daa5
alex <hortinstein@gmail.com>
2019-03-17 13:31:01 -0400

    added relevat documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
+use std::fs;
 use std::process::{self, Command};
 
-extern crate  test_runner;
-extern crate  skeleton_parser;
+extern crate fs_extra;
+extern crate skeleton_parser;
+extern crate test_runner;
+extern crate hubcaps;
 
-use skeleton_parser::{SkeletonCode,SkeletonDelimiters};
+use fs_extra::copy_items;
+use fs_extra::dir::copy;
+use fs_extra::dir::CopyOptions;
+use skeleton_parser::{SkeletonCode, SkeletonDelimiters};
 use test_runner::broker_test;
+use hubcaps::{Credentials, Github};
 
 //returns a command setup ready to run the tests
-fn setup_command(test_command: &str, command_directory: &str) -> Command {
+fn command_wrapper(test_command: &str, command_directory: &str) -> Command {
     let mut command = if cfg!(target_os = "windows") {
         {
             let mut c = Command::new("cmd");
@@ -26,12 +33,36 @@ fn setup_command(test_command: &str, command_directory: &str) -> Command {
     command
 }
 
+pub fn duplicate_directory(src: &str, dest: &str) {
+    let options = CopyOptions::new(); //Initialize default values for CopyOptions
+
+    // copy dir1 and file1.txt to target/dir1 and target/file1.txt
+    let mut from_paths = Vec::new();
+    from_paths.push(src);
+    copy_items(&from_paths, dest, &options).unwrap();
+}
+
+pub struct GithubCommand {
+    token: String
+}
+
+impl GithubCommand{
+    pub fn set_github_token(&self, name:&str) {
+        let github = Github::new(
+            "myreplicatedu/0.0.1",
+            Credentials::Token(self.token.clone()),
+        );
+        
+
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn echo_hello() {
-        let mut c = setup_command("ls", "..");
+        let mut c = command_wrapper("ls", "..");
         dbg!(c.output());
     }
 }
