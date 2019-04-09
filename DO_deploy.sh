692d5178ff79282b0db770463aff26f65d814f8c
alex <hortinstein@gmail.com>
2019-04-08 22:00:25 -0400

    added script

--- DO_deploy.sh
+++ DO_deploy.sh
@@ -3,8 +3,9 @@ sudo apt-get install build-essential git
 curl https://sh.rustup.rs -sSf | sh
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-mkdir replicatEdu
-git clone git clone https://github.com/replicatedu/replicatedu_lib
-git clone git clone https://github.com/replicatedu/class_register
-git clone git clone https://github.com/replicatedu/class_database
-
+mkdir -p replicatEdu
+git clone https://github.com/replicatedu/replicatedu_lib replicatEdu/replicatedu_lib 
+git clone https://github.com/replicatedu/class_register replicatEdu/class_register
+git clone https://github.com/replicatedu/class_database replicatEdu/class_database
+wget https://github.com/codercom/code-server/releases/download/1.691-vsc1.33.0/code-server1.691-vsc1.33.0-linux-x64.tar.gz
+tar code-server1.691-vsc1.33.0-linux-x64.tar.gz
